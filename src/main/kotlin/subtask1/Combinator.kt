27b479c77495a97e0cdd73d5272fc169961d30ff
Vitali Shukevich <v.shukevich@iomico.com>
2022-04-19 10:28:26 +0300

    1task

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -4,6 +4,28 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val targetBinomC = array[0]
+        val n = array[1]
+        var k = 1
+        while (k < n){
+            val currentBinomC = binomC(n,k)
+            if (currentBinomC == targetBinomC) return k
+            k++
+        }
+        return null
+    }
+
+    fun factorial(n: Int): Double {
+        var result = 1.0
+        for (i in 1..n) {
+            result *= i
+        }
+        return result
+    }
+
+    fun binomC(n: Int, k: Int): Int {
+
+        return (factorial(n) / (factorial(k) * factorial(n - k))).toInt()
+
     }
 }
