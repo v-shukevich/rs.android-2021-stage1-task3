49f3fcabfcf79e61116b0f7024c92052c31ce4c0
Vitali Shukevich <v.shukevich@iomico.com>
2022-04-20 10:59:39 +0300

    3 task done

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -4,6 +4,25 @@ class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        return decompose(square(number),number-1)
     }
+
+    private fun decompose(numberSqr: Int, startNumber: Int): Array<Int>? {
+        for (i in startNumber downTo 1) {
+            val reminder = numberSqr - square(i)
+            when {
+                reminder == 0 -> return arrayOf(i)
+                reminder < 0 -> return null
+                else -> {
+                    var nextNumber = kotlin.math.sqrt(reminder.toDouble()).toInt()
+                    if (nextNumber >= i) nextNumber = i - 1
+                    val result = decompose(reminder, nextNumber)
+                    if (result != null) return result + arrayOf(i)
+                }
+            }
+        }
+        return null
+    }
+
+    private fun square(i: Int) = i * i
 }
--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -4,6 +4,63 @@ class TelephoneFinder {
 
     // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+        var result = emptyArray<String>()
+        if (number.isNotEmpty() && number.all { it.isDigit() }) {
+            for (i in number.indices) {
+                when (number[i]){
+
+                    '1' ->{
+                        result += number.replace(number[i],'2')
+                        result += number.replace(number[i],'4')
+                    }
+                    '2' ->{
+                        result += number.replace(number[i],'1')
+                        result += number.replace(number[i],'5')
+                        result += number.replace(number[i],'3')
+                    }
+                    '3' ->{
+                        result += number.replace(number[i],'2')
+                        result += number.replace(number[i],'6')
+                    }
+                    '4' ->{
+                        result += number.replace(number[i],'1')
+                        result += number.replace(number[i],'5')
+                        result += number.replace(number[i],'7')
+                    }
+                    '5' ->{
+                        result += number.replace(number[i],'2')
+                        result += number.replace(number[i],'4')
+                        result += number.replace(number[i],'6')
+                        result += number.replace(number[i],'8')
+                    }
+                    '6' ->{
+                        result += number.replace(number[i],'3')
+                        result += number.replace(number[i],'5')
+                        result += number.replace(number[i],'9')
+                    }
+                    '7' ->{
+                        result += number.replace(number[i],'4')
+                        result += number.replace(number[i],'8')
+                    }
+                    '8' ->{
+                        result += number.replace(number[i],'5')
+                        result += number.replace(number[i],'7')
+                        result += number.replace(number[i],'9')
+                        result += number.replace(number[i],'0')
+                    }
+                    '9' ->{
+                        result += number.replace(number[i],'6')
+                        result += number.replace(number[i],'8')
+                    }
+                    else ->{
+                        result += number.replace(number[i],'8')
+                    }
+
+                }
+            }
+            return result
+        }
+
+        return null
     }
 }
